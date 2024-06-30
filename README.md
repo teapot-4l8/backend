ecf75c00e7297198ba46d0b294898681c98b3e5c
teapot-4l8 <chaiyunze@gmail.com>
2024-06-30 23:29:33 +0800

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+## pip install
+pip install django
+pip install djangorestframework
+pip install django-cors-headers
+
+## start
+```
+python manage.py runserver
+```
--- backends/personal/views.py
+++ backends/personal/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import personal
+from .models import *
 from .serializers import personalSerializer
 
 
 class personalViewSet(viewsets.ModelViewSet):
     queryset = personal.objects.all().order_by('-username')
     serializer_class = personalSerializer
+
+
+def get_table_data(request):
+    """
+    获取文本遗忘表格的数据
+    """
+    # 从数据库中获取数据
+    data_from_db = YourModel.objects.all()
+
+    # 将数据序列化为JSON格式
+    serialized_data = [{'field1': item.field1, 'field2': item.field2} for item in data_from_db]
+
+    # 返回JSON响应
+    return JsonResponse(serialized_data, safe=False)def get_data(request):
+    # 从数据库中获取数据
+    data_from_db = YourModel.objects.all()
+
+    # 将数据序列化为JSON格式
+    serialized_data = [{'field1': item.field1, 'field2': item.field2} for item in data_from_db]
+
+    # 返回JSON响应
+    return JsonResponse(serialized_data, safe=False)
